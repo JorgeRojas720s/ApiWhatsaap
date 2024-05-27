e778fa46541989a10932ef462d25d6768d155818
Yzma01 <marchena.isma@gmail.com>
2024-05-26 19:05:04 -0600

    layout over image set, only works for me

--- api-whatsapp/src/App.jsx
+++ api-whatsapp/src/App.jsx
@@ -1,24 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
     <div>
       <img />
-      <div className='form-conteiner'>
-        <input className='number'/>
-        <input className='message' type='textAre'/>
-        <input className='location'/>
-        <button className='get-location'>
-          <svg className='location-map'>
-
-          </svg>
-        </button>
-        <button className='send'>
-
-        </button>
+      <div className="form-conteiner">
+        <input className="number" placeholder="Numver" type="number"/>
+        <textarea className="message" placeholder="Messach" />
+        <div>
+          <input className="location" readOnly placeholder="Lokachon" />
+          <button className="get-location" onClick={()=> alert('get-location')}></button>
+        </div>
+        <button className="send" onClick={()=> alert('zent')}></button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
