2195d5eb23aa8a6897cff2117ec3d36f37b79249
JorgeRojas720s <jitorojas37@gmail.com>
2024-05-30 11:53:30 -0600

    api key

--- api-whatsapp/src/App.jsx
+++ api-whatsapp/src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import ReverseGeocoding from './MapsApi/ReverseGeocoding'
 
 const App = () => {
   return (
     <div>
       {/* <img src="./assets/images/APIbgAll.jpg" /> */}
+      <ReverseGeocoding/>
+{/* 
       <div className="form-conteiner">
         <input className="number" placeholder="Numver" type="number" />
         <textarea className="message" placeholder="Messach" />
@@ -15,7 +18,7 @@ const App = () => {
           ></button>
         </div>
         <button className="send" onClick={() => alert("zent")}></button>
-      </div>
+      </div> */}
     </div>
   );
 };
